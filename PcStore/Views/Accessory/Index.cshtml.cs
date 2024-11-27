ea2d0f8a7ef483c6e8e6cd88daf120819fe86efe
TeodorAngelov1 <teodorangelov93@gmail.com>
2024-11-28 00:12:35 +0200

    add accessory page views

---
+++ PcStore/Views/Accessory/Index.cshtml.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PcStore.Web.Views.Accessory
+
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
